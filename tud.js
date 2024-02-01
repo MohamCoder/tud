4c72766072b57327bfdcc2b31545c5b2b07666bb
MohamCoder <125498068+MohamCoder@users.noreply.github.com>
2024-02-01 17:04:44 +0300

    Update tud.js

--- tud.js
+++ tud.js
@@ -1,17 +1,21 @@
 fileDir = process.argv[2];
+const MAIN_DIR = fileDir[0].toLowerCase();
 if (!fileDir) {
   console.log('No file specified');
+  process.exit();
+}
+if (fileDir.includes("/")) {
+  console.log('not valid windows path');
   process.exit();
 }
 if (fileDir.startsWith("/mnt")) {
   console.log(fileDir);
   process.exit();
 }
-if (fileDir[1] === ":" && !fileDir.includes("\\")) {
-  console.log('wrap ur file with ""');
+if (fileDir[1] === ":" && !fileDir.includes("\\") && fileDir.length > 2) {
+  console.log('wrap ur file with ""')
   process.exit();
 }
-const MAIN_DIR = fileDir[0].toLowerCase();
 fileDir = fileDir.split("\\");
 if (fileDir[0].includes(":")) fileDir[0] = `/mnt/${MAIN_DIR}`;
 console.log(fileDir.join("/"));
